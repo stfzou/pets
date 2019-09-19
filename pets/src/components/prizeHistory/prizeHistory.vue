e644ea9aa4a960d45c6a8a90b37442599ab2cfd2
Mr Zou <1007151493@qq.com>
2019-09-19 18:24:22 +0800

    2019-9-19

--- pets/src/components/prizeHistory/prizeHistory.vue
+++ pets/src/components/prizeHistory/prizeHistory.vue
@@ -57,8 +57,8 @@
       }
     },
     mounted() {
-      this.type = this.getParamValue('type');
-      this.userId = this.getParamValue('userId');
+      this.type = this.getUrlKey('type');
+      this.userId = this.getUrlKey('userId');
       this.getDataList();
 
 
@@ -81,37 +81,8 @@
       		alert('这个是ios操作系统')
       	}
       },
-      getParamValue(name) {
-            var paramsArray = this.getUrlParams();
-            if (paramsArray != null) {
-              for (var i = 0 ; i < paramsArray.length ; i++) {
-                for (var j in paramsArray[i]) {
-                  if (j == name) {
-                    return paramsArray[i][j];
-                  }
-                }
-              }
-            }
-
-            return null;
-      },
-      getUrlParams() {
-          var search = window.location.search;
-          // 写入数据字典
-          var tmparray = search.substr(1, search.length).split("&");
-          var paramsArray = new Array;
-          if (tmparray != null) {
-            for (var i = 0; i < tmparray.length; i++) {
-              var reg = /[=|^==]/;  // 用=进行拆分，但不包括==
-              var set1 = tmparray[i].replace(reg, '&');
-              var tmpStr2 = set1.split('&');
-              var array = new Array;
-              array[tmpStr2[0]] = tmpStr2[1];
-              paramsArray.push(array);
-            }
-          }
-          // 将参数数组进行返回
-          return paramsArray;
+      getUrlKey(name){
+          return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
       },
       showBtn() {
             let self = this;
